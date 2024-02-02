908d2388cbccd9001dfe176e9947136b91d5a370
f32917 <pralay.patoria@us.bnpparibas.com>
2024-02-02 00:39:51 -0500

    test

--- generate_resume.sh
+++ generate_resume.sh
@@ -1,4 +1,4 @@
-echo "generating resume.pdf from resume.md..."
+echo "generating resume.pdf from resume.md...."
 echo "pandoc resume.md -f markdown+yaml_metadata_block --template templates/jb2resume.latex -o resume.pdf"
 pandoc resume.md -f markdown+yaml_metadata_block --template templates/jb2resume.latex -o resume.pdf
-echo "completed..."
+echo "completed...."
